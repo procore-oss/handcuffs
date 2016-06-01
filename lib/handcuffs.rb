b868944c8000340b31b41b29432a906c45aaa342
Brad Urani <bradurani@gmail.com>
2016-06-01 13:19:43 -0700

    Add handcuffs module declaration to handcuffs.rb

--- lib/handcuffs.rb
+++ lib/handcuffs.rb
@@ -1,6 +1,8 @@
 require 'rails'
 require 'active_record'
 
+module Handcuffs; end
+
 Dir[File.join(File.dirname(__FILE__), 'handcuffs', '*.rb')].each {|file| require file }
 
 ActiveRecord::Migration.extend Handcuffs::Extensions
